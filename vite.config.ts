3a28b7d7688c51c88208aa67757f8cbbd7a4f240
ImRLopezAG <angelg00lopez@gmail.com>
2025-04-02 18:24:44 -0400

    refactor: remove unused configuration files and update index.html for script import

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite"
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@components': resolve(__dirname, './src/_components'),
-      '@hooks': resolve(__dirname, './src/_hooks'),
-      '@services': resolve(__dirname, './src/_services'),
-      '@ui': resolve(__dirname, './src/_components/ui'),
-      '@shared': resolve(__dirname, './src/shared')
-    }
-  }
+  plugins: [react(), tailwindcss(), viteTsConfigPaths()],
 })
